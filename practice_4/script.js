95d84bf5d522d0844211276a68b9afdfc19ba0d0
Chandan Ojha <chandanojha776@gmail.com>
2022-09-10 11:32:41 +0600

    practice_4 complete

---
+++ practice_4/script.js
@@ -0,0 +1,25 @@
+function loadData(){
+  //create a new request
+  const xhr = new XMLHttpRequest();
+
+  //what to do when response arrives
+  xhr.onload = function(){
+    const container = document.getElementById("demo");
+    container.innerHTML = xhr.responseText;
+    //console.log(this.getAllResponseHeaders());
+    console.log(this.getResponseHeader("etag"));
+  };
+
+  //prepare requests - methods: GET, POST, PUT, PATCH, DELETE,OPTIONS
+  xhr.open("GET","./data/data.txt",false);
+
+  //add a request header
+  //xhr.setRequestHeader("My_Fav_Language","Javascript");
+
+  //send request
+  xhr.send();
+
+
+  //abort
+  //xhr.abort()
+}
